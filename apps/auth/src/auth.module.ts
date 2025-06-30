811994b99943f8f775efd31b2af06c2050a9cdc0
Mohammed Alsaeed <saeedalsaeedbusiness@gmail.com>
2025-06-30 21:45:27 +0300

    Fix db connection issue

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -18,6 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true,
       }),
     }),
+    TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
